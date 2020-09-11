6ba1406ef99525042dcba22dea55f25a426fa776
FujiHaruka <fizzyjohn1427@gmail.com>
2020-09-11 23:33:59 +0900

    wip

--- test/lcom.test.js
+++ test/lcom.test.js
@@ -1,23 +1,31 @@
-const rule = require('../lib/lcom')
-const { RuleTester } = require('eslint')
+const rule = require("../lib/lcom");
+const { RuleTester } = require("eslint");
 
-const tester = new RuleTester()
+const tester = new RuleTester();
 
-const parserOptions = { ecmaVersion: 6 }
+const parserOptions = { ecmaVersion: 6 };
 
 tester.run("lcom", rule, {
   valid: [
-    // { code: "class A { constructor() {} }", parserOptions },
-    { code: "class A { sum() { return this.a + this.b } diff() { const {a, b} = this; return a - b } }", parserOptions },
-    // { code: "class A { sum() { const s = this; return s } }", parserOptions },
+    { code: "class A { constructor() {} }", parserOptions },
+    {
+      code:
+        "class A { sum() { return this.a + this.b } diff() { const {a, b} = this; return a - b } get aa() { return this.a } }",
+      parserOptions,
+    },
+    {
+      code:
+        "class A { aa() { const {a: b = 1 } = this; return b } bb() { this.aa() } }",
+      parserOptions,
+    },
   ],
   invalid: [
-  //   {
-  //     code: "class A { foo() {} }",
-  //     parserOptions: { ecmaVersion: 6 },
-  //     errors: [
-  //         { type: "FunctionExpression", line: 1, column: 14, messageId: "missingThis", data: { name: "method 'foo'" } }
-  //     ]
-  // },
-  ]
-})
+    //   {
+    //     code: "class A { foo() {} }",
+    //     parserOptions: { ecmaVersion: 6 },
+    //     errors: [
+    //         { type: "FunctionExpression", line: 1, column: 14, messageId: "missingThis", data: { name: "method 'foo'" } }
+    //     ]
+    // },
+  ],
+});
