1faa2cd1366c7d62e39a512c4ecef7a3fb60bbf2
FujiHaruka <fizzyjohn1427@gmail.com>
2020-09-12 10:03:32 +0900

    Refactor calcLOCM function

--- test/graph.test.js
+++ test/graph.test.js
@@ -1,15 +1,18 @@
 const assert = require("assert").strict;
 const Graph = require("../lib/graph");
+const { flat } = require("../lib/utils");
 
 describe("Graph", () => {
   it("constructor()", () => {
-    const graph = new Graph([
+    const edges = [
       ["a", "b"],
       ["b", "c"],
       ["d", "a"],
       ["e", "f"],
       ["f", "e"],
-    ]);
+    ];
+    const nodes = new Set(flat(edges));
+    const graph = new Graph(nodes, edges);
     assert.deepEqual(
       graph.edgeMap,
       new Map([
@@ -25,52 +28,108 @@ describe("Graph", () => {
 
   it("countConnectedComponents()", () => {
     {
+      const edges = [
+        ["a", "b"],
+        ["b", "c"],
+        ["c", "a"],
+        ["d", "a"],
+      ];
+      const nodes = new Set(flat(edges));
+      assert.equal(new Graph(nodes, edges).countConnectedComponents(), 1);
+    }
+    {
+      const edges = [
+        // component 1
+        ["a", "b"],
+        ["c", "d"],
+        // component 2
+        ["e", "f"],
+        ["f", "g"],
+      ];
+      const nodes = new Set(flat(edges));
+      assert.equal(new Graph(nodes, edges).countConnectedComponents(), 3);
+    }
+    {
+      const edges = [
+        // component 1
+        ["a", "b"],
+        ["b", "c"],
+        ["c", "d"],
+        // component 2
+        ["e", "f"],
+        ["g", "f"],
+        ["h", "g"],
+        ["i", "e"],
+        // component 3
+        ["x", "y"],
+        ["y", "z"],
+        // component 4
+        ["o", "o1"],
+        ["o", "o2"],
+        ["o", "o3"],
+        ["o", "o4"],
+        ["o2", "o4"],
+      ];
+      const nodes = new Set(flat(edges));
+      assert.equal(new Graph(nodes, edges).countConnectedComponents(), 4);
+    }
+  });
+
+  it("static calcLCOM()", () => {
+    {
+      assert.equal(Graph.calcLCOM([]), 0);
+    }
+    {
       assert.equal(
-        new Graph([
-          ["a", "b"],
-          ["b", "c"],
-          ["c", "a"],
-          ["d", "a"],
-        ]).countConnectedComponents(),
+        Graph.calcLCOM([
+          {
+            name: "method1",
+            properties: ["a", "b"],
+          },
+          {
+            name: "method2",
+            properties: ["c", "b"],
+          },
+        ]),
         1
       );
     }
     {
       assert.equal(
-        new Graph([
-          // component 1
-          ["a", "b"],
-          ["c", "d"],
-          // component 2
-          ["e", "f"],
-          ["f", "g"],
-        ]).countConnectedComponents(),
-        3
+        Graph.calcLCOM([
+          {
+            name: "method1",
+            properties: ["a", "b"],
+          },
+          {
+            name: "method2",
+            properties: ["c", "b"],
+          },
+          {
+            name: "method3",
+            properties: ["d"],
+          },
+        ]),
+        2
       );
     }
     {
       assert.equal(
-        new Graph([
-          // component 1
-          ["a", "b"],
-          ["b", "c"],
-          ["c", "d"],
-          // component 2
-          ["e", "f"],
-          ["g", "f"],
-          ["h", "g"],
-          ["i", "e"],
-          // component 3
-          ["x", "y"],
-          ["y", "z"],
-          // component 4
-          ["o", "o1"],
-          ["o", "o2"],
-          ["o", "o3"],
-          ["o", "o4"],
-          ["o2", "o4"],
-        ]).countConnectedComponents(),
-        4
+        Graph.calcLCOM([
+          {
+            name: "method1",
+            properties: ["method3"],
+          },
+          {
+            name: "method2",
+            properties: ["method3"],
+          },
+          {
+            name: "method3",
+            properties: ["a"],
+          },
+        ]),
+        1
       );
     }
   });
